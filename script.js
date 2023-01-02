b333b8cbb6a954cc4aeb0861ce3c4488c7130e82
Pratik Senapati <90540195+CriMsonTYpo@users.noreply.github.com>
2023-01-02 19:36:19 +0530

    Add all functionalities
    Completed :)

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 let yes=document.querySelector("#container");
 let s=false;
+let b=document.querySelector("#b");
+let colour=document.querySelector("#colour");
+let c="black";
 
 
 function create(num)
 {
     let k=500/num;
-    let u=k.toString();
+    
     for(let i=0;i<num;i++)
     {
         for(let j=0;j<num;j++)
@@ -27,7 +30,7 @@ function create(num)
             {
                 if(s==true)
                 {
-                    cell.style.backgroundColor="black"; 
+                    cell.style.backgroundColor=c; 
                 }
             })
             cell.addEventListener("mouseup",function()
@@ -45,4 +48,28 @@ function create(num)
 }
 
 create(16);
+function clear()
+{
+    let u=document.querySelectorAll(".c");
+    u.forEach(element => {
+        yes.removeChild(element)
+        
+    });
+}
 
+b.addEventListener("click", function(event)
+{
+    clear();
+    let number=prompt("Please enter the size of the grid","16");
+    if(+number<1 || +number>100)
+    {
+        alert("Please enter a number between 1 and 1000");
+        return;
+    }
+    create(+number);
+})
+
+colour.addEventListener("change", function()
+{
+    c=colour.value;
+})
